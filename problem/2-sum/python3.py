020d4c1e16795fae915f2f1acbfb7922fea29f36
Yang Jiang <yangjiang@YangdeMacBook-Pro.local>
2024-09-15 11:31:29 +1200

    na

--- problem/2-sum/python3.py
+++ problem/2-sum/python3.py
@@ -1,3 +1,35 @@
+# Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+# You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+# You can return the answer in any order.
+
+
+# Example 1:
+
+# Input: nums = [2,7,11,15], target = 9
+# Output: [0,1]
+# Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+# Example 2:
+
+# Input: nums = [3,2,4], target = 6
+# Output: [1,2]
+# Example 3:
+
+# Input: nums = [3,3], target = 6
+# Output: [0,1]
+
+
+# Constraints:
+
+# 2 <= nums.length <= 104
+# -109 <= nums[i] <= 109
+# -109 <= target <= 109
+# Only one valid answer exists.
+
+
+# Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?
+
 
 def two_sum(nums: list[int], target: int) -> list[int]:
     num_to_index = {}  # Dictionary to store numbers and their indices
@@ -5,10 +37,10 @@ def two_sum(nums: list[int], target: int) -> list[int]:
     for i, num in enumerate(nums):
         complement = target - num
         if complement in num_to_index:
-            return [num_to_index[complement], i] 
+            return [num_to_index[complement], i]
         num_to_index[num] = i
 
     return []  # No solution found
 
 
-print(two_sum([1,2,3,4], 3))
+print(two_sum([1, 2, 3, 4], 3))
